ffe5096ad73f9d9654da8115b800eaf2433c369e
Tyler hostager <tyhostager@tyler-hs-macbook-pro.local>
2017-06-07 11:07:12 -0500

    Update: added view creation upon execution

--- src/ModsInstallerDriver.java
+++ src/ModsInstallerDriver.java
@@ -1,13 +1,23 @@
+import com.sun.istack.internal.NotNull;
+
+import javax.swing.*;
+
 /**
  * The driver for the application
  *
  * Created by tyhostager on 6/7/17.
  */
 public class ModsInstallerDriver {
+
+
     public static void main(String[] args) {
         MIModel model = new MIModel();
         MIView view = MIView.init();
-        MIController controller = new MIController(model, view);
 
+        init(model, view);
+    }
+
+    private static void init(@NotNull MIModel model, @NotNull MIView view) {
+        SwingUtilities.invokeLater(() -> new MIController(model, view));
     }
 }
